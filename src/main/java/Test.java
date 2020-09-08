5dd573b3c239e31ea7838e33dcf01f40b0878996
neeraj.gaur <HulufB@6N>
2020-09-07 17:06:48 -0700

    initial commit

---
+++ src/main/java/Test.java
@@ -0,0 +1,11 @@
+public class Test {
+
+    public static void main(String args[])
+    {
+        int width=5;
+        int height=10;
+        int area=width*height;
+        System.out.println("Area of rectangle="+area);
+    }
+
+}
